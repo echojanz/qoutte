acb58d17a8eda629dd447d04e94ea9cc58590956
Uğur Oruç <uoruc5@hotmail.com>
2017-12-10 21:17:53 +0300

    fonts updated

--- src/api/fonts.js
+++ src/api/fonts.js
@@ -10,6 +10,150 @@ const Fonts = [
 	},
 	{
 		name: 'Arvo'
+	},
+	{
+		name: 'Abel'
+	},
+	{
+		name: 'Abril Fatface'
+	},
+	{
+		name: 'Arimo'
+	},
+	{
+		name: 'Barlow:500'
+	},
+	{
+		name: 'Berkshire Swash'
+	},
+	{
+		name: 'Bitter'
+	},
+	{
+		name: 'Bree Serif'
+	},
+	{
+		name: 'Cabin'
+	},
+	{
+		name: 'Cabin Sketch'
+	},
+	{
+		name: 'Catamaran'
+	},
+	{
+		name: 'Coustard'
+	},
+	{
+		name: 'Crimson Text:600'
+	},
+	{
+		name: 'Didact Gothic'
+	},
+	{
+		name: 'Domine'
+	},
+	{
+		name: 'Fauna One'
+	},
+	{
+		name: 'Faustina'
+	},
+	{
+		name: 'Grand Hotel'
+	},
+	{
+		name: 'Hind'
+	},
+	{
+		name: 'Karla'
+	},
+	{
+		name: 'Leckerli One'
+	},
+	{
+		name: 'Libre Baskerville'
+	},
+	{
+		name: 'Lora'
+	},
+	{
+		name: 'Lustria'
+	},
+	{
+		name: 'Merriweather:700'
+	},
+	{
+		name: 'Montserrat:700'
+	},
+	{
+		name: 'Muli:200'
+	},
+	{
+		name: 'Nunito:600'
+	},
+	{
+		name: 'Oswald'
+	},
+	{
+		name: 'Oxygen'
+	},
+	{
+		name: 'PT Sans:700'
+	},
+	{
+		name: 'PT Serif:700'
+	},
+	{
+		name: 'Patua One'
+	},
+	{
+		name: 'Permanent Marker'
+	},
+	{
+		name: 'Playfair Display:700'
+	},
+	{
+		name: 'Poppins:500'
+	},
+	{
+		name: 'Prata'
+	},
+	{
+		name: 'Quicksand'
+	},
+	{
+		name: 'Raleway'
+	},
+	{
+		name: 'Roboto Slab:100'
+	},
+	{
+		name: 'Robot:300'
+	},
+	{
+		name: 'Rock Salt'
+	},
+	{
+		name: 'Rubik'
+	},
+	{
+		name: 'Satisfy'
+	},
+	{
+		name: 'Slabo'
+	},
+	{
+		name: 'Source Sans Pro:600'
+	},
+	{
+		name: 'Unica One'
+	},
+	{
+		name: 'Varela Round'
+	},
+	{
+		name: 'Work Sans'
 	}
 ];
 
